5e0c873cefabec6f10f4d215bd489c0bba5d304d
AdusaRisingSun <Adusa@AdusadeMac-mini.local>
2015-08-25 15:54:43 +0800

    Initial Commit

---
+++ TouchTrackerView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TouchTrackerView
+//
+//  Created by Adusa on 15/8/25.
+//  Copyright (c) 2015年 Adusa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
